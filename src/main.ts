d5c1e8ab0d9cb534d5586fbd0fc1267b2c872712
nhat <pmnhat.17ck1@gmail.com>
2023-05-27 17:20:51 +0700

    allow cros

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // Cấu hình CORS
+   const corsOptions: CorsOptions = {
+    origin: '*', // Cho phép tất cả các tên miền
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  };
   const config = new ConfigService();
+  app.enableCors(corsOptions);
   await app.listen(await config.getPortConfig());
 }
 bootstrap();
