5df97dfa99db8cf409338b4e5c1632df4b8f51f7
nhat <pmnhat.17ck1@gmail.com>
2023-05-27 20:09:25 +0700

    add more field

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -6,13 +6,16 @@ export type UserDocument = User & Document;
 @Schema()
 export class User {
   @Prop({ required: true })
-  name: string;
+  username: string;
 
   @Prop({ required: true })
   password: string;
 
   @Prop({ required: true })
   email: string;
+
+  @Prop({ required: true })
+  birthday: string;
 
   @Prop({ required: true })
   phone: string;
